f6eb588838379a8f865d84945302a6fed7e71f5b
Zahwah Al-Hosni <zahwahth@gmail.com>
2022-09-18 13:06:48 +0400

    fixed responsive design layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,12 +6,12 @@ const Home = (props) => {
 
   return (
     <>
-      <h1><span className="garamond-font">Congratulations</span> on beginning your job search! This app will allow you to prepare for a <span className="garamond-font">variety</span> of <span className="biau-font">interview</span> questions</h1>
+      <h1 className ="home-page_txt"><span className="garamond-font">Congratulations</span> on beginning your job search! This app will allow you to prepare for a <span className="garamond-font">variety</span> of <span className="biau-font">interview</span> questions</h1>
 
       {/* Button */}
       <div className="home-page_btns">
-      <button className="black-btn" onClick={() => navigate("/instructions")}><span className="garamond-font">check</span> instructions</button>
-      <button style={{color: 'black'}} onClick={() => navigate("/category")}><span className="garamond-font">let's</span> start!</button>
+      <button className="black-btn home-btn" onClick={() => navigate("/instructions")}><span className="garamond-font">check</span> instructions</button>
+      <button className="home-btn" style={{color: 'black'}} onClick={() => navigate("/category")}><span className="garamond-font">let's</span> start!</button>
       </div>
 
     </>
